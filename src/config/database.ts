1ca9860f40ab04d4c3a58893bb2e532337f0d8a5
denis junior <denis_jr2001@hotmail.com>
2025-08-22 15:21:30 -0300

    Refactor database configuration to use DATABASE_URL for connection details

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,12 +5,11 @@ import { Seller } from "../entities/Seller";
 import { Function } from "../entities/Functions";
 
 export const AppDataSource = new DataSource({
-  type: "postgres", // ou 'mysql', dependendo do seu banco
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necessário para Railway
+  },
   synchronize: process.env.NODE_ENV === "development",
   logging: process.env.NODE_ENV === "development",
   entities: [User, Client, Seller, Function],
